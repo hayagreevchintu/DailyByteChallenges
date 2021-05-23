05401438b849ebdff5080685eae7173142dc95da
Hayagreev <47315980+hayagreevchintu@users.noreply.github.com>
2021-05-23 17:53:28 +0530

    Update longestCommonPrefix.py

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,11 +1,16 @@
 '''
+
+Question:
 Given an array of strings, return the longest common prefix that is shared amongst all strings.
 Note: you may assume all strings only contain lowercase alphabetical characters.
 Ex: Given the following arrays:
 ["colorado", "color", "cold"], return "col"
 ["a", "b", "c"], return ""
 ["spot", "spotty", "spotted"], return "spot"
+
 '''
+
+#Solution begins here:
 def longestCommonPrefix(strings):
     if len(strings) == 0:
         return ""
