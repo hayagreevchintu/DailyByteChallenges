a7d9e54b259808228dd148bf41d2186a9fcf74cc
Hayagreev <47315980+hayagreevchintu@users.noreply.github.com>
2021-05-23 17:54:27 +0530

    Update vaccumCleanerRoute.py

--- vaccumCleanerRoute.py
+++ vaccumCleanerRoute.py
@@ -1,4 +1,6 @@
 '''
+
+Question:
 Given a string representing the sequence of moves a robot vacuum makes, return whether or not it will return to its original position.
 The string will only contain L, R, U, and D characters, representing left, right, up, and down respectively.
 Ex: Given the following strings:
@@ -7,6 +9,8 @@ Ex: Given the following strings:
 "RUULLDRD", return true
 
 '''
+
+#Solution begins here:
 def vaccumCleanerRoute(route):
     routeDict = {'U' : 0, 'D' : 0, 'L' : 0, 'R' : 0}
     for i in route:
